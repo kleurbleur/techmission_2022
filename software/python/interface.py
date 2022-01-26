ff283f8a9bd084fc63283cd5c15dd52b262304f1
Kleurbleur <info@kleurbleur.nl>
2022-01-26 13:07:41 +0100

    multi slider and counters

--- software/python/interface.py
+++ software/python/interface.py
@@ -5,7 +5,8 @@ import json
 import time
 
 app = Flask(__name__)
-counter = 100
+generator_counter = 100
+battery_counter = 10
 
 ##############################
 @app.route("/")
@@ -18,10 +19,16 @@ def listen():
 
   def respond_to_client():
     while True:
-      global counter
-      print(counter)
-      counter += 1
-      _data = json.dumps({"counter":counter})
+      global generator_counter
+      global battery_counter
+      print('gen_counter: ', generator_counter)
+      print('batt_counter: ', battery_counter)
+      generator_counter += 1
+      battery_counter += 2
+      _data = json.dumps({
+        "generator_counter": generator_counter,
+        "battery_counter": battery_counter
+        })
       yield f"id: 1\ndata: {_data}\nevent: online\n\n"
       time.sleep(0.1)
   return Response(respond_to_client(), mimetype='text/event-stream')
